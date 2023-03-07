d897e84ea12038a4783531a64a3a8a81e74c79eb
stevgeniy <stepin_evgen@mail.ru>
2023-03-07 21:10:15 +0300

    RANDOM

--- Exampl_004/Program.cs
+++ Exampl_004/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 13;
-double numberB = 5;
-Console.WriteLine (numberA / numberB);
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
+Console.WriteLine (numberA + numberB);
